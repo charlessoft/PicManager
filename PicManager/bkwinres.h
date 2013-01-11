93f518eb95948b9784fded9049b05e14ece03127
charlessoft <charlessoft@qq.com>
2013-01-11 17:41:26 +0800

    new porject

---
+++ PicManager/bkwinres.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#define IDC_BTN_SYS_CLOSE			100
+#define IDC_BTN_MAX					101
+#define IDC_BTN_MIN					102
+#define IDC_BTN_MENU				114
+#define IDC_CHK_CHK1				4
+#define IDC_CHK_MYCHK				18
+#define IDS_APP_FONT				1
+
+#define IDC_BTN_ADD					1023
+#define IDC_BTN_EDIT				1024
+#define IDC_BTN_DELETE				1025
+#define IDC_BTN_START_CHANGE		1026
+
+#define IDC_EDIT_CTRL				2007
+
+#define IDC_LISTVIEW_CTRL			2001
+
+
+#define IDC_TESTTEXT					1030
+#define IDC_DATIKA					1031
--- PicManager/stdafx.h
+++ PicManager/stdafx.h
@@ -11,16 +11,38 @@
 #define _WIN32_IE	0x0501
 #define _RICHEDIT_VER	0x0200
 
+// #include <atlbase.h>
+// #include <atlapp.h>
+// 
+// extern CAppModule _Module;
+// 
+// #include <atlwin.h>
+// 
+// #include <atlframe.h>
+// #include <atlctrls.h>
+// #include <atldlgs.h>
+
 #include <atlbase.h>
-#include <atlapp.h>
-
-extern CAppModule _Module;
-
+//#pragma comment(lib, "atlthunk.lib")
 #include <atlwin.h>
+#include <atlstr.h>
+#include <atltrace.h>
 
-#include <atlframe.h>
-#include <atlctrls.h>
-#include <atldlgs.h>
+#define _WTL_NO_CSTRING
+
+#include <wtl/atlapp.h>
+#include <wtl/atlframe.h>
+#include <wtl/atlctrls.h>
+#include <wtl/atlctrlx.h>
+#include <wtl/atldlgs.h>
+#include <wtl/atlcrack.h>
+#include <wtl/atlmisc.h>
+
+#include <tinyxml/tinyxml.h>
+#include <bkwin/bkdlgview.h>
+#include <bkres/bkres.h>
+//#include "bkwinres.h"
+extern CAppModule _Module;
 
 #if defined _M_IX86
   #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
